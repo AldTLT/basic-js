d3a9c6872cf270ebc5e5484224ef59f0add5fb3f
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-02-22 00:08:55 +0400

    feat: add dream-team algorithm

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members) ? members
+    .filter(x => (typeof x) == 'string')
+    .map(z => z
+      .trimLeft()
+      .toUpperCase()[0]
+    )
+    .sort()
+    .join('') : false;
 };
