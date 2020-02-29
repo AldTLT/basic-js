820d37c23ee19414eb084e081bdad27c12070721
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-02-29 19:47:48 +0400

    feat: add recursive depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,14 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, depth) {
+        depth === undefined ? depth = 1 : depth++;
+        let ndepth = depth;
+        arr.forEach(x => {
+            if (Array.isArray(x)) {
+                let calcDepth = this.calculateDepth(x, depth);
+                ndepth = ndepth > calcDepth ? ndepth : calcDepth;
+            };
+        });
+    
+        return ndepth;
     }
 };
