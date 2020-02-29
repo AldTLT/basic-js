8d168b8de4fa0b04fc2bd222549796ff8158e967
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-02-29 19:16:49 +0400

    feat: add extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (str === undefined) {
+        return str;
+    }
+
+    let repTimes = options.repeatTimes === undefined ? 1 : options.repeatTimes;
+    let sep = repTimes <= 1 ? '' : options.separator === undefined ? '+' : options.separator;
+    let add = options.addition === undefined ? '' : options.addition + '';
+    let addRepTimes = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+    let addSep = options.additionSeparator === undefined ? '|' : options.additionSeparator;
+
+    let additional = (add + addSep).repeat(addRepTimes).slice(0, -addSep.length);
+
+    let result = (str + '' + additional + sep).repeat(repTimes);
+    return result.slice(0, result.length - sep.length);
 };
-  
