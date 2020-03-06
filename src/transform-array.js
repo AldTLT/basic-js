ec3f3ce23ca99d86cd84ecbe80d2b470192d20a4
Yuriy Ulivanov <vr0rtex@mail.ru>
2020-03-06 09:55:02 +0400

    feat: add transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,42 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error;
+    }
+
+    let result = [];
+
+    let index = 0;
+    while (index < arr.length) {
+
+        switch (arr[index]) {
+            case '--discard-next':
+                {
+                    index++;
+                    break;
+                }
+            case '--discard-prev':
+                {
+                    result.pop();
+                    break;
+                }
+            case '--double-next':
+                {
+                    result.push(arr[index + 1]);
+                    break;
+                }
+            case '--double-prev':
+                {
+                    result.push(arr[index - 1]);
+                    break;
+                }
+            default:
+                {
+                    result.push(arr[index]);
+                }
+        }
+
+        index++;
+    }
+
+    return result.filter(x => x !== undefined);
 };
